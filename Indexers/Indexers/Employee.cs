0785b87fa29added1dbe10db1fe743238701c360
Rifat392000 <arrifat392000@outlook.com>
2024-04-06 20:31:25 +0600

    Indexers

--- Indexers/Indexers/Employee.cs
+++ Indexers/Indexers/Employee.cs
@@ -6,7 +6,88 @@ using System.Threading.Tasks;
 
 namespace Indexers
 {
-    internal class Employee
+    public class Employee
     {
+        int Eno;
+        double Salary;
+        string Ename, Job, Dname, Location;
+
+        public Employee(int Eno, double Salary, string Ename, string Job, string Dname, string Location)
+        {
+            this.Eno = Eno;
+            this.Salary = Salary;
+            this.Ename = Ename;
+            this.Job = Job;
+            this.Dname = Dname;
+            this.Location = Location;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+
+                if (index == 1)
+                {
+                    return Eno;
+                }
+
+                else if (index == 2)
+                {
+                    return Salary;
+
+                }
+
+                else if (index == 3)
+                {
+                    return Ename;
+                }
+
+                else if (index == 4)
+                {
+                    return Location;
+                }
+
+                else if (index == 5)
+                {
+                    return Job;
+                }
+
+
+                else if (index == 6)
+                {
+                    return Dname;
+                }
+
+                return null;
+
+            }
+            set {
+
+                if (index == 1)
+                {
+                    Eno = (int)value;
+                }
+
+                else if (index == 2)
+                {
+                    Salary = (double)value;
+
+                }
+
+                else if (index == 3)
+                {
+                    Ename = (string)value;
+                }
+
+
+                else if (index == 6)
+                {
+                     Dname = (string)value;
+                }
+
+
+            }
+        }
     }
 }
--- Indexers/Indexers/Program.cs
+++ Indexers/Indexers/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee emp = new Employee(1001, 2447.24, "Rifat", "Engineer", "System Designer", "Osaka");
+
+            Console.WriteLine(emp[1]);
+            Console.WriteLine(emp[2]);
+            Console.WriteLine(emp[3]);
+            Console.WriteLine(emp[4]);
+            Console.WriteLine(emp[5]);
+            Console.WriteLine(emp[6]);
+
+            Console.WriteLine();
+            emp[1] = 200;
+            emp[2] = 500000.23;
+            Console.WriteLine(emp[1]);
+            Console.WriteLine(emp[2]);
+
+            emp[3] = "Alis";
+
+            Console.WriteLine(emp[3]);
+
+            Console.WriteLine(emp[4]);
+            Console.WriteLine(emp[5]);
+
+            emp[6] = "Rocket Engineer";
+            Console.WriteLine(emp[6]);
         }
     }
 }
