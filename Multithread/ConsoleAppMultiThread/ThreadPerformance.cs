c28f49ab7e1c33af1d965986918bd51ac66b2bd3
Rifat392000 <arrifat392000@outlook.com>
2024-04-18 20:54:39 +0600

    Multi Thread

---
+++ Multithread/ConsoleAppMultiThread/ThreadPerformance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleAppMultiThread
+{
+    internal class ThreadPerformance
+    {
+        static long count1 = 0 , count2 =0;
+        static void Increment1()
+        {
+            for(long i = 0; i < 100000000; i++)
+            {
+                count1 += 1;
+            }
+        }
+
+        static void Increment2()
+        {
+            for (long i = 0; i < 100000000; i++)
+            {
+                count2 += 1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw1 = new Stopwatch();
+            Stopwatch sw2 = new Stopwatch();
+
+
+            sw1.Start();
+            Increment1();
+            Increment2();
+            sw1.Stop();
+
+            Thread t1 = new Thread(Increment1);
+            Thread t2 = new Thread(Increment2);
+
+            sw1.Start();
+            t1.Start();
+            t2.Start();
+            sw2.Stop();
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine($"Single Thread Timelapse {sw1.ElapsedMilliseconds}");
+            Console.WriteLine($"Multi Thread Timelapse {sw2.ElapsedMilliseconds}");
+
+        }
+    }
+}
---
+++ Multithread/ConsoleAppMultiThread/ThreadPriorities.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleAppMultiThread
+{
+    internal class ThreadPriorities
+    {
+        static long count1 = 0, count2 = 0;
+        static CancellationTokenSource cts = new CancellationTokenSource();
+
+        static void Counter1()
+        {
+            while (true)
+            {
+                if (cts.Token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Counter1 is exiting.");
+                    break;
+                }
+                count1++;
+            }
+        }
+
+        static void Counter2()
+        {
+            while (true)
+            {
+                if (cts.Token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Counter2 is exiting.");
+                    break;
+                }
+                count2++;
+            }
+        }
+
+        static void Main()
+        {
+            Thread t1 = new Thread(Counter1);
+            Thread t2 = new Thread(Counter2);
+            t1.Priority = ThreadPriority.Lowest;
+            t2.Priority = ThreadPriority.Highest;
+            t1.Start();
+            t2.Start();
+
+            Console.WriteLine("Main thread going to sleep");
+            Thread.Sleep(5000);
+            Console.WriteLine("Main thread woke up");
+
+            cts.Cancel(); // Request cancellation
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine("Count 1 " + count1);
+            Console.WriteLine("Count 2 " + count2);
+            Console.WriteLine("Main Thread Exit");
+        }
+    }
+}
